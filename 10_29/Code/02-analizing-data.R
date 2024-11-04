37aecc64c882b17529d0d2423bda4b4ced744cce
JosefinaSilva96 <josefinasilvafuente@gmail.com>
2024-11-04 11:26:16 -0500

    changes

--- 10_29/Code/02-analizing-data.R
+++ 10_29/Code/02-analizing-data.R
@@ -9,6 +9,7 @@ library(modelsummary)
 library(stargazer)
 library(ggplot2)
 library(tidyr)
+library(RColorBrewer)
 
 # Load data 
 
@@ -73,35 +74,26 @@ print(p)
 # Save the plot
 ggsave(output_path_graphs, plot = p, width = 8, height = 6)
 
+#Graph number of genres per year
 
-# Bar graph number of projects for projects types
+pastel_colors <- c("#FFB3BA", "#FFDFBA", "#FFFFBA", "#BAFFC9", "#BAE1FF",
+                   "#FFABAB", "#FFC3A0", "#FF677D", "#D4A5A5", "#392F5A",
+                   "#B9FBC0", "#C1C8E4", "#FFC4D6", "#F6B93B", "#B1E1FF",
+                   "#A6D8D4", "#FFE156", "#D3C6E5", "#B1C3E7", "#F0A500",
+                   "#F9AFAF", "#A6D7C0")  # Adding two more colors
+# Summarize the number of movies by year and genre
 
-output_path_graphs <- file.path("C:/WBG/GitHub/Tidy_Tuesday-s/10_29/Outputs/graph_genres_numvotes.png")
+summary_data <- data_table[, .(num_movies = .N), by = .(year, genre_1)] 
 
-# Filter and plot using ggplot2
-p <- data_table %>%  
-  ggplot(aes(x = gdpPercap, y = lifeExp, size = pop, color = continent)) +
-  geom_point() +
-  theme_bw()
-
+# Create the plot with pastel colors
+p1 <- ggplot(summary_data, aes(x = year, y = num_movies, color = genre_1)) +
+  geom_point(size = 3) +        # Use points for each year
+  theme_minimal() +             # Clean theme
+  labs(title = "Number of Movies by Genre Across Years",
+       x = "Year",
+       y = "Number of Movies",
+       color = "Genre") +
+  scale_color_manual(values = pastel_colors)  # Using custom pastel colors
+  
 # Print the plot
-print(p)
-
-# Save the plot
-ggsave(output_path_graphs, plot = data_table_plot, width = 8, height = 6)
-
-
-# Graph for projects and time
-
-output_path_graphs <- file.path("C:/WBG/GitHub/Bangladesh/Outputs/graph_project_time.png")
-
-data_table3_plot <- data_table3 %>%
-  ggplot(aes(x = fiscal_year_id)) + 
-  geom_bar(fill = "#B3CDE3") +
-  geom_text(stat = "count", aes(label = ..count..), vjust = -0.3) +  # Add count labels
-  labs(title = "Project Time Distribution", x = "Fiscal Year", y = "Count") +
-  theme_minimal()
-
-
-# Save the plot
-ggsave(output_path_graphs, plot = data_table3_plot, width = 8, height = 6)
+print(p1)
